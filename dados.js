aa5f57b3a8149eac26e29bfae9527d7971120837
Sapper <alesaty@gmail.com>
2024-09-07 17:12:55 -0300

    Create dados.js
    Contém informações para o funcionamento do sistema de pesquisa

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+        titulo: "Red Dead Redemption 2",
+        descricao: "Red Dead Redemption 2 é um jogo de mundo aberto que te transporta para o fim do século XIX, no coração do Velho Oeste americano.",
+        link: "https://pt.wikipedia.org/wiki/Red_Dead_Redemption_2",
+        tags: "red dead redemption redemtion ded"
+    },
+    {
+        titulo: "Psychonauts 2",
+        descricao: "Psychonauts 2 é um jogo super divertido e cheio de mistérios! Você vai acompanhar Raz, um garoto com poderes psíquicos incríveis, em uma jornada por mundos malucos dentro das mentes de outras pessoas.",
+        link: "https://pt.wikipedia.org/wiki/Psychonauts_2",
+        tags: "pisiconauts psychonauts"
+    },
+    {
+        titulo: "Grand Theft Auto IV",
+        descricao: "Grand Theft Auto IV te coloca nas ruas de Liberty City, uma cidade inspirada em Nova York. Você controla Niko Bellic, um imigrante em busca do sonho americano.",
+        link: "https://pt.wikipedia.org/wiki/Grand_Theft_Auto_IV",
+        tags: "gta 4 IV"
+    }
+];
